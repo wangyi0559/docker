4264fdf16c651d55e5b2f8f5c4c324e013e58cd5
wangyi0559 <969392953@qq.com>
2017-10-18 10:58:16 +0800

    未起sdk时，获取高度为空时bug

--- zabbix/zabbix-agent/getHeight.sh
+++ zabbix/zabbix-agent/getHeight.sh
@@ -2,7 +2,7 @@
 function getHeight(){
     RESULT=$(curl -s 127.0.0.1:8080/api/getAll)
     SUCCESS=$(echo $RESULT | jq ".success" | sed "s/\"//g")
-    if [ $SUCCESS == "true" ]
+    if [ [$SUCCESS] == "true" ]
     then
         HEIGHT=$(echo $RESULT | jq ".height" | sed "s/\"//g")
         echo $HEIGHT > /height
