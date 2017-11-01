7ceeb387d573d68c99ba19dee324aad430dfb6bb
wangyi0559 <969392953@qq.com>
2017-11-01 17:39:57 +0800

    网络获取 传输简化

--- zabbix/zabbix-agent/getNet.sh
+++ zabbix/zabbix-agent/getNet.sh
@@ -3,7 +3,11 @@ function getNet(){
     cp /netdat /netdat1
     >/netdat
     sed -i '1d;$d' /netdat1
-    cat /netdat1
+    awk '{ $1=null;$3=null;$5=null;print }' /netdat1 > /netdat2
+    awk '{a[$1" "$2]+=$3;}END{for (i in a) print i,a[i];}' /netdat2>/netdat3
+    cat /netdat3
     rm /netdat1
+    rm /netdat2
+    rm /netdat3
 }
 getNet
