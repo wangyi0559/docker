488fc6358ec87e90e47e5e4277455c51dc3fb134
wangyi0559 <969392953@qq.com>
2017-11-02 14:11:20 +0800

    网络监控

--- zabbix/zabbix-agent/getNet.sh
+++ zabbix/zabbix-agent/getNet.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 AGENT_IP=`ifconfig  | grep 'inet addr:' | grep -v '127.0.0.1' | grep -v '0.0.0.0' | grep -v '172.' | cut -d: -f2 | awk '{ print $1}'`
 function getNet(){
-    cp /netdat /netdat1
-    >/netdat
-    sed -i '1d;$d' /netdat1
-    awk '{ $1=null;$3=null;$5=null;print }' /netdat1 > /netdat2
-    awk '{a[$1" "$2]+=$3;}END{for (i in a) print i,a[i];}' /netdat2>/netdat3
-    sed -i -e "/$AGENT_IP/!d" /netdat3
-    sed -i "s/$AGENT_IP//g" /netdat3
-    cat /netdat3
-    rm /netdat1
-    rm /netdat2
-    rm /netdat3
+    cp /chain/netdat /chain/netdat1
+    >/chain/netdat
+    sed -i '1d;$d' /chain/netdat1
+    awk '{ $1=null;$3=null;$5=null;print }' /chain/netdat1 > /chain/netdat2
+    awk '{a[$1" "$2]+=$3;}END{for (i in a) print i,a[i];}' /chain/netdat2>/chain/netdat3
+    sed -i -e "/$AGENT_IP/!d" /chain/netdat3
+    sed -i "s/$AGENT_IP//g" /chain/netdat3
+    cat /chain/netdat3
+    rm /chain/netdat1
+    rm /chain/netdat2
+    rm /chain/netdat3
 }
 getNet
--- zabbix/zabbix-agent/startNet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ETH_ID=$(ifconfig -s | awk '{print $1}' | grep "^e")
-
-#nohup tcpdump -qn  -i $ETH_ID  port not  22  > /netdat 2>&1 &
-
-nohup tcpdump -qtn  -i $ETH_ID 'tcp port not 22 and port not 10050 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' > /netdat 2>&1 &
