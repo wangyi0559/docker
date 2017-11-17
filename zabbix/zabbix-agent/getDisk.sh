a8d1561b0cadb97b40ad9f05fb35c4236aac3e05
wangyi0559 <969392953@qq.com>
2017-11-17 16:51:53 +0800

    获取磁盘数据去除干扰项

--- zabbix/zabbix-agent/getDisk.sh
+++ zabbix/zabbix-agent/getDisk.sh
@@ -10,19 +10,19 @@ if [ ! -f "/chain/PEER_INDEX" ]; then
 fi
 PEER_INDEX=$(cat /chain/PEER_INDEX)
 case $PEER_INDEX in
-        1)   CONTAINER_NAME="orderer.example.com" 
+        1)   CONTAINER_NAME=" orderer.example.com" 
         ;;
-        2)   CONTAINER_NAME="ca_peerOrg1" 
+        2)   CONTAINER_NAME=" ca_peerOrg1" 
         ;;
-        3)   CONTAINER_NAME="ca_peerOrg2" 
+        3)   CONTAINER_NAME=" ca_peerOrg2" 
         ;;
-        4)   CONTAINER_NAME="peer0.org1.example.com" 
+        4)   CONTAINER_NAME=" peer0.org1.example.com" 
         ;;
-        5)   CONTAINER_NAME="peer1.org1.example.com" 
+        5)   CONTAINER_NAME=" peer1.org1.example.com" 
         ;;
-        6)   CONTAINER_NAME="peer0.org2.example.com" 
+        6)   CONTAINER_NAME=" peer0.org2.example.com" 
         ;;
-        7)   CONTAINER_NAME="peer1.org2.example.com" 
+        7)   CONTAINER_NAME=" peer1.org2.example.com" 
         ;;
         *)   echo 'error' >/dev/null 2>&1 
         ;;
