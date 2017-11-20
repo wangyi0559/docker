02abed60478ee38144c93b681cf61bf5573f2b05
wangyi0559 <969392953@qq.com>
2017-11-20 17:25:39 +0800

    block 解析

--- zabbix/zabbix-agent/getBlock.sh
+++ zabbix/zabbix-agent/getBlock.sh
@@ -4,10 +4,22 @@ function getBlock(){
     NOW=$(cat /now)
     if test $[NOW] -lt $[HEIGHT]
     then
-        RESULT=$(curl -s 127.0.0.1:8080/api/getInfo?blockId=$NOW)
         NEW=`expr $NOW + 1`
+        BLOCK_ID=$NOW
+        BLOCK_HEIGHT=`expr $BLOCK_ID + 1`
+        RESULT=$(curl -s 127.0.0.1:8080/api/getInfo?blockId=$BLOCK_ID)
+        BLOCK_IP=$(cat /chain/AGENT_IP)
+        BLOCK_HASH=$(echo $RESULT | jq '.header.data_hash')
+        BT_NUM=$(echo $RESULT | jq '.data.data|length')
+        BLOCK_TRANSACTIONS="[]"
+        for((i=0;i<$BT_NUM;i++));  
+        do   
+            BT_TRANSACTIONID=$(echo $RESULT | jq ".data.data[$i].payload.header.channel_header.tx_id")
+            BT_CREATIONTIME=$(echo $RESULT | jq ".data.data[$i].payload.header.channel_header.timestamp")
+            BLOCK_TRANSACTIONS=$(echo $BLOCK_TRANSACTIONS | jq ".[$i].transactionid=$BT_TRANSACTIONID|.[$i].creationtime=$BT_CREATIONTIME")
+        done
+        echo "{}" | jq ".ip=\"$BLOCK_IP\"|.blockid=\"$BLOCK_ID\"|.height=$BLOCK_HEIGHT|.hash=$BLOCK_HASH|.transactions=$BLOCK_TRANSACTIONS"
         echo $NEW > /now
-        echo $RESULT
     else
        sleep 10s
     fi
